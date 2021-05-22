03ca2def1474ce8db1b90dcbbb0065f9c556dc83
HguitarH <hanan.hazani@gmail.com>
2021-05-22 18:58:39 +0300

    Add project files.

---
+++ StackOverflowQuestions/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflowQuestions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
