41704f3173954f84a3b607a717fc32850ae135f1
HguitarH <hanan.hazani@gmail.com>
2021-05-22 19:21:25 +0300

    FasterLinqQuery

---
+++ StackOverflowQuestions/FasterLinqQuery.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StackOverflowQuestions
+{
+    public class FasterLinqQuery
+    {
+        public class A
+        {
+            public DateTime Date;
+        }
+
+        public void solution(List<A> listObject, DateTime asOfDate)
+        {
+            var maxDate = listObject.Max(r => r.Date <= asOfDate ? r.Date : DateTime.MinValue);
+
+            var item = listObject.FirstOrDefault(r => r.Date == maxDate);
+        }
+    }
+}
